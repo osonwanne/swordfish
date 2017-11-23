bd7ef02fc3f80de37553933d0726538677fa6deb
Bon Osonwanne <nosonwan@gmail.com>
2017-11-22 19:47:09 -0700

    started parse page

--- swordfish/spiders/spider.py
+++ swordfish/spiders/spider.py
@@ -19,3 +19,32 @@ class MySpider(scrapy.Spider):
 	    nextpage = response.xpath('//a[@class="u-decoration-none next pagination-links_anchor"]/@href').extract_first() 
 	    request1 = scrapy.Request(response.urljoin(nextpage))
 	    yield request1
+
+	# def parse_page(self, response):
+	# 	item = SwordfishItem()
+
+	#     name = response.xpath('//h1[@class="biz-page-title embossed-text-white shortenough"]/text()').extract_first()
+	    
+	#     #introduce case or if/else
+	#     menu = response.xpath('//a[@class="external-menu js-external-menu"]/@href').extract_first()
+
+	#     # menu = response.xpath('//a[@class="menu-explore js-menu-explore"]/@href').extract_first()
+	#     # menu = "www.yelp.com"+menu
+
+	#     #click on full menu link
+
+
+	#     swordfish = response.xpath('//li[@id="region-zipcode"]/a/text()').extract_first()
+
+	#     item['name'] = self.manual(name)
+	#     item['url'] = self.manual(response.url)
+	#     item['menu'] = self.manual(menu)
+	#     item['swordfish'] = self.manual(swordfish)
+
+	#     return item
+
+
+    def manual(self,var_str):
+        if var_str is None:
+            return ""
+        return var_str
